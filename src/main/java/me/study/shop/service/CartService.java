9ef6ad0fdf586b3feb87491caf214e2d7a5177af
pch8388 <pch838811@gmail.com>
2020-06-07 01:08:03 +0900

    [feat] cart 저장 추가 - service

---
+++ src/main/java/me/study/shop/service/CartService.java
@@ -0,0 +1,38 @@
+package me.study.shop.service;
+
+import lombok.RequiredArgsConstructor;
+import me.study.shop.domain.Cart;
+import me.study.shop.domain.Member;
+import me.study.shop.domain.Product;
+import me.study.shop.exception.NotFoundMemberException;
+import me.study.shop.exception.NotFoundProductException;
+import me.study.shop.repository.CartRepository;
+import me.study.shop.repository.MemberRepository;
+import me.study.shop.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class CartService {
+
+	private final CartRepository cartRepository;
+	private final MemberRepository memberRepository;
+	private final ProductRepository productRepository;
+
+	@Transactional
+	public Cart saveCart(Long memberId, Long productId) {
+		final Member member = memberRepository.findById(memberId)
+			.orElseThrow(NotFoundMemberException::new);
+
+		final Product product = productRepository.findById(productId)
+				.orElseThrow(NotFoundProductException::new);
+
+		return cartRepository.save(
+			Cart.builder()
+				.member(member)
+				.product(product)
+				.build());
+	}
+}
