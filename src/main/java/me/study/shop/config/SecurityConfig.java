3db1cb3bf793b8d144ace86513f6f6f1ae502730
pch8388 <pch838811@gmail.com>
2020-09-09 00:19:11 +0900

    [feat] security config 적용

---
+++ src/main/java/me/study/shop/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package me.study.shop.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers(
+            "/swagger-resources", "/webjars/**",
+            "/static/**", "/templates/**", "/h2/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .csrf().disable()
+            .headers().disable()
+            .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+            .authorizeRequests()
+                .antMatchers("/api/v1/member").permitAll()
+                .antMatchers("/api/v1/categories/").hasRole("ROLE_ADMIN")
+                .antMatchers("/api/**").hasRole("ROLE_USER")
+                .anyRequest().permitAll()
+                .and()
+            .formLogin().disable();
+    }
+}
