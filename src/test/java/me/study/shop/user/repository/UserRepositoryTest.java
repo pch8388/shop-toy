5a49c5038634a3dbfd39ce4453489376f83b5502
pch8388 <pch838811@gmail.com>
2021-01-19 10:57:46 +0900

    [test] User entity 에 BaseEntity 적용 테스트
    시간 등록 성공
    등록자 등록 실패
    
    등록자를 SecurityContext 에서 꺼내와서 넣어주기 위한 처리 필요

---
+++ src/test/java/me/study/shop/user/repository/UserRepositoryTest.java
@@ -0,0 +1,45 @@
+package me.study.shop.user.repository;
+
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import me.study.shop.user.domain.Address;
+import me.study.shop.user.domain.Email;
+import me.study.shop.user.domain.User;
+
+@DataJpaTest
+class UserRepositoryTest {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	private User saveUser;
+
+	@BeforeEach
+	void setUp() {
+		final String username = "tester";
+		final String password = "1234";
+		final Email email = new Email("test@test.com");
+		final Address address = new Address("Seoul", "street", "12345");
+
+		saveUser = userRepository.save(
+			User.createUser(username, password, email, address));
+	}
+
+	@Test
+	@DisplayName("auditing 테스트")
+	void baseTimeEntity() {
+		LocalDateTime now = LocalDateTime.now();
+
+		assertThat(saveUser.getCreateDate()).isBefore(now);
+		assertThat(saveUser.getCreatedBy()).isNotNull();
+	}
+}
